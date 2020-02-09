59761f1bd10a0302c858d4090b19d3f8ca98ba13
suhasisthere <suhasisthere@gmail.com>
2020-02-09 20:25:01 +0530

    Update unitsAndGlobal.sol

--- unitsAndGlobal.sol
+++ unitsAndGlobal.sol
@@ -17,3 +17,34 @@ contract unitsAndGlobal{
         
         return (gasleft(),msg.value);
     }
+    
+    function getSignature() public pure returns (bytes4){
+        return msg.sig;
+    }
+    
+    function getBlockNumber() public view returns (uint){
+        return block.number;
+    }
+    function getDifficulty() public view returns (uint){
+        //return block.difficulty;
+        return block.gaslimit;
+    }
+    //returns the address of the miner who mined the last block
+    function getCoinbase() public view returns (address){
+        return block.coinbase;
+    }
+    function getTxprice() public view returns (uint){
+        return tx.gasprice;
+    }
+    //to get the hash of the value
+   function getHash(bytes memory _val) public pure returns (bytes32){
+      return sha256(_val);
+   }
+   function getBalance(address _add) public view returns (uint){
+       return _add.balance;
+   }
+   function sendEther(address payable _to_address) payable public  returns(bool){
+       _to_address.transfer(msg.value);
+       return true;
+   }
+}
